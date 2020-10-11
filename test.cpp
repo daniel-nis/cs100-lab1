457dbfe62933926c9e78748b5e870fa31167d351
daniel-nis <danielnissan211@gmail.com>
2020-10-11 11:11:40 -0700

    Added second test that works with blank input

--- test.cpp
+++ test.cpp
@@ -7,6 +7,11 @@ TEST(EchoTest, HelloWorld) {
     EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+	char* test_val[1]; test_val[0] = "./c-echo";
+	EXPECT_EQ("", echo(1,test_val));
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
